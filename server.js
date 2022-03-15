7a8482508dbfefe8013933d4ae6159c06b2d1fe1
umutdurna <umutdurna@sameup.co>
2022-03-15 10:32:42 +0300

    umut geri döndü

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const express = require('express');
 var port = 3000;
 var app = express();
 var wikiUrls1 = "";
-// umut recep durna
-// pull request
-//umut önce umut sonra maine 
-// umut tekrardan geliyor
-
-// goooaosdodasoasd
+// Artık yeni başlangıç
 rp(url)
   .then(function(html){
     //success!
